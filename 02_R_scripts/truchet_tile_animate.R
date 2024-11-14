58751af9371228c74c64d28070129126bca30d6c
Allan James <allanjames1506@gmail.com>
2024-11-14 22:22:39 +0000

    first commits

---
+++ 02_R_scripts/truchet_tile_animate.R
@@ -0,0 +1,11 @@
+
+# 1. LIBRARIES----
+
+# install.packages("devtools")
+devtools::install_github("paezha/truchet")
+
+library(dplyr)
+library(gganimate)
+library(ggplot2)
+library(sf)
+library(truchet)
